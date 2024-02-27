7dee1a2bb39e6aaa0287e82b4a838509c940f14f
kentigerc <157593216+kentigerc@users.noreply.github.com>
2024-02-27 05:59:06 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Asking for user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Printing personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
